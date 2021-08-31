8a08945e82973ec24f345e6e39f68ffe597df89d
AndreaDipa <andreadipaa@gmail.com>
2021-08-31 19:16:16 +0200

    add authorization in views for exams

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -6,9 +6,32 @@ class ExamsController < ApplicationController
         @exam = Exam.find(params[:id])
     end
     def new
-        @project = Project.new
+        @exam = Exam.new
     end
     def create
+        exam = Exam.new(params.require(:exam).permit(:name, :code, :description))
+        if exam.save
+            flash[:notice] = "Exam created successfully"
+            current_user.exams << exam
+            redirect_to exams_path
+        else
+            flash[:alert] = "Something wrong"
+            redirect_to 'new'
+
+        end
+    end
+    def edit
+        @exam = Exam.find(params[:id])
+    end
+    def update
+        exam = Exam.find(params[:id])
+        if exam.update(params.require(:exam).permit(:name, :code, :description))
+            flash[:notice] = "Exam was updated successfully"
+            redirect_to exam
+        else
+            flash[:alert] = "Something wrong"
+            render 'edit'
+        end
     end
 
 end
