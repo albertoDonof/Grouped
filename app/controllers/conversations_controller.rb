5a03f2ffc175d081ffcbb0be3c732b738c02a55d
AndreaDipa <andreadipaa@gmail.com>
2021-09-16 17:55:51 +0200

    conversation style

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,9 +1,12 @@
 class ConversationsController < ApplicationController
     def index
         @conversations = current_user.mailbox.conversations
+        
     end
     def show
+        
         @conversation = current_user.mailbox.conversations.find(params[:id])
+
     end
     def new
         @recipient = User.find(params[:user_id])
@@ -13,5 +16,10 @@ class ConversationsController < ApplicationController
         receipt = current_user.send_message(recipient, params[:body], params[:subject])
         redirect_to conversation_path(receipt.conversation)
 
+    end
+    def destroy
+        conversation = current_user.mailbox.conversations.find(params[:id])
+        conversation.destroy
+        redirect_to conversations_path
     end
 end
